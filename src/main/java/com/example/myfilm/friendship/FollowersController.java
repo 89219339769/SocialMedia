9049beeddbce76a23db6033b1a7f0deb71cc6938
slava rudakas <username@yandex.ru>
2023-05-30 18:14:58 +0300

    add post activity

---
+++ src/main/java/com/example/myfilm/friendship/FollowersController.java
@@ -0,0 +1,30 @@
+package com.example.myfilm.friendship;
+
+
+import com.example.myfilm.post.model.Post;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping()
+public class FollowersController {
+
+private final FriendshipService friendshipService;
+    @GetMapping("/getActivity")
+    public List<Post> getActivity ()  {
+
+        return friendshipService.addActivity();
+
+    }
+
+
+}
--- src/main/java/com/example/myfilm/friendship/FriendshipService.java
+++ src/main/java/com/example/myfilm/friendship/FriendshipService.java
@@ -1,6 +1,7 @@
 package com.example.myfilm.friendship;
 
 
+import com.example.myfilm.post.model.Post;
 import com.example.myfilm.user.UserEntity;
 import com.example.myfilm.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -30,7 +33,9 @@ public class FriendshipService {
         UserEntity friend2 = userRepository.findById(Math.toIntExact(friendId))
                                            .orElseThrow(() -> new RuntimeException(
                                                    "user with id = " + friendId + " not found"));
-
+        if (friendId == userFr.get().getId()) {
+            throw new RuntimeException("you can't be friends with yourself");
+        }
         followers.setFollower1(userFr.get());
         followers.setFollower2(friend2);
         followersRepo.save(followers);
@@ -45,15 +50,35 @@ public class FriendshipService {
                                            .orElseThrow(() -> new RuntimeException(
                                                    "user with id = " + friendId + " not found"));
 
-        Integer UserFr =  userFr.get().getId();
-        Integer UserFr2 =  friend2.getId();
-        Followers follower = followersRepo.findByFolIdAndId(UserFr2, UserFr)
-                                          .orElseThrow(() -> new RuntimeException(
-                "user with id = " + friendId + " not found"));
-        followersRepo.delete(follower);
+        Integer UserFr = userFr.get().getId();
+        Integer UserFr2 = friend2.getId();
+        followersRepo.findByFolIdAndId(UserFr2, UserFr)
+                     .orElseThrow(() -> new RuntimeException("user with id = " + friendId + " not found"));
+
         Friendship friendship = new Friendship();
         friendship.setFriend1(userFr.get());
         friendship.setFriend2(friend2);
         friendshipRepo.save(friendship);
+    }
+
+    public List<Post> addActivity() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+        Optional<UserEntity> userFr = userRepository.findByUsername(currentUserName);
+
+        List<Followers> followers = followersRepo.findByFr(userFr.get().getId());
+        List<Integer> postsId = new ArrayList<>();
+        for (var follower : followers) {
+            postsId.add(follower.getFollower2().getId());
+        }
+        List<Post>usPosts = new ArrayList<>();
+
+        //найти не по номеру пользователей а по username
+        //взять посты из базы и у которых совпадает username в список постов класть
+        List<UserEntity> users = userRepository.findAllById(postsId);
+        for (var user : users) {
+            usPosts.add(user.)
+
+        }
     }
 }
